b9e01bda400655bd4c304d56a3ab4654b034a666
Fidelsalazar <89423945+Fidelsalazar@users.noreply.github.com>
2022-11-11 09:33:39 -0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+  profesor = 'Andres Guzman';
+  curso = 'Curso Spring con Angular 14';
+}
